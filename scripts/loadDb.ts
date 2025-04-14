9884938b12a7303119a1c8bcbfca2fc20b9d4913
sxy15 <sxy_19921115@163.com>
2025-04-14 22:42:20 +0800

    feat: init

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -3,3 +3,28 @@ import { PuppeteerWebBaseLoader } from '@langchain/community/document_loaders/we
 import OpenAI from 'openai'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import "dotenv/config"
+
+const { 
+    ASTRA_DB_NAMESPACE, 
+    ASTRA_DB_COLLECTION, 
+    ASTRA_DB_API_ENDPOINT, 
+    ASTRA_DB_APPLICATION_TOKEN,
+    OPENAI_API_KEY
+} = process.env
+
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY
+})
+
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One',
+    'https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship',
+]
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN)
+const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE })
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 512,
+    chunkOverlap: 100,
+})
